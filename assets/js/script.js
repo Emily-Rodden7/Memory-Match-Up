4a692cdb82784e81b1ba3ac8d7b4621e7004eda3
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-03-04 19:55:17 +0000

    add in face card images to js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,15 @@ let isMatch = 0;
 
 // Card images
 const getData = () => [
-    {name: "ElephantCard", image: "assets\images\ElephantCard.png"}
-]
+    {name: "ElephantCard", image: "assets\images\ElephantCard.png"},
+    {name: "GiraffeCard", image: "assets\images\GiraffeCard.png"},
+    {name: "GorillaCard", image: "assets\images\GorillaCard.png"},
+    {name: "HippoCard", image: "assets\images\LeopardCard.png"},
+    {name: "LionFaceCard", image: "assets\images\LionFaceCard.png"},
+    {name: "MonkeyCard", image: "assets\images\MonkeyCard.png"},
+    {name: "SnakeCard", image: "assets\images\SnakeCard.png"},
+    {name: "TocoToucanCard", image: "assets\images\TocoToucanCard.png"}
+];
 
 // randomizing cards
 const randomize = () => {
