d73f431fc34c47dc3d9f0fa4c109688ad11db931
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-03-05 16:57:50 +0000

    editing js to fix bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const gridContainer = document.querySelector(".grid-container";)
+const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let firstCard, secondCard;
 let lockBoard = false;
@@ -6,13 +6,18 @@ let errors = 0;
 
 document.querySelector(".errors").textContent = error;
 
-fetch("assets\images")
-.then((res) => res.images())
-.then((data) => {
-    cards = [...data, ...data];
-    shuffleCards();
-    generateCards();
-});
+// Card images
+const images = [
+    {name: "ElephantCard", image: "assets\images\ElephantCard.png"},
+    {name: "GiraffeCard", image: "assets\images\GiraffeCard.png"},
+    {name: "GorillaCard", image: "assets\images\GorillaCard.png"},
+    {name: "HippoCard", image: "assets\images\LeopardCard.png"},
+    {name: "LionFaceCard", image: "assets\images\LionFaceCard.png"},
+    {name: "MonkeyCard", image: "assets\images\MonkeyCard.png"},
+    {name: "SnakeCard", image: "assets\images\SnakeCard.png"},
+    {name: "TocoToucanCard", image: "assets\images\TocoToucanCard.png"}
+];
+let cards = [...images, ...images];
 
 function shuffleCards() {
     let currentIndex = cards.length,
@@ -93,15 +98,3 @@ function shuffleCards() {
         generateCards();
     }
 }
-
-// Card images
-const images = [
-    {name: "ElephantCard", image: "assets\images\ElephantCard.png"},
-    {name: "GiraffeCard", image: "assets\images\GiraffeCard.png"},
-    {name: "GorillaCard", image: "assets\images\GorillaCard.png"},
-    {name: "HippoCard", image: "assets\images\LeopardCard.png"},
-    {name: "LionFaceCard", image: "assets\images\LionFaceCard.png"},
-    {name: "MonkeyCard", image: "assets\images\MonkeyCard.png"},
-    {name: "SnakeCard", image: "assets\images\SnakeCard.png"},
-    {name: "TocoToucanCard", image: "assets\images\TocoToucanCard.png"}
-];
